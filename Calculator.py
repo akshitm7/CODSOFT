6c1f9272d2115cd4de4e512707090ab77c933f3b
Akshit Kumar Mishra <64767895+akshitm7@users.noreply.github.com>
2024-07-23 21:24:59 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,1 +1,33 @@
+n = int(input("Enter the number of values:"))
+values = []
+for i in range(n):
+    values.append(int(input("Enter the value:")))
+
+sum_values = 0
+product_values = 1
+difference_values = values[0]
+def divide_values(values):
+    result=values[0]
+    for value in values[1:]:
+        if value==0:
+            print("Divide by zero error.")
+            return None
+        result /=value
+    return result
+
+
+for value in values:
+    sum_values += value
+    product_values *= value
+
+for value in values[1:]:
+    difference_values -= value
+
+division_result=divide_values(values)
+
+print("Sum:", sum_values)
+print("Product:", product_values)
+print("Difference:", difference_values)
+if division_result is not None:
+    print("Division:", division_result)
 
